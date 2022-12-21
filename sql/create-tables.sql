6214b17f51e17fca3b30e7a50aa998239105f7a9
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-12-21 12:50:13 +0200

    working on database

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,8 +1,46 @@
-# 
-REATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
+-- CREATE TABLES SQL
+-- Kovács Bálit-Hunor
+-- Informatika II. D
+
+-- TOTAL TABLES: 27
+
+-- 1. Publisher 
+CREATE SEQUENCE publ_seq START WITH 1;
+
+CREATE TABLE Publishers (
+  publisher_id              NUMBER(10)  DEFAULT publ_seq.nextval NOT NULL,
+  publisher_name          	VARCHAR2(255)  NOT NULL);
+
+ALTER TABLE Publishers ADD (
+  CONSTRAINT publ_pk PRIMARY KEY (publisher_id));
+COMMIT;
+
+ALTER TABLE Publishers
+ADD publisher_formationDate DATE;
+
+-- 2. Producers 
+CREATE SEQUENCE prod_seq START WITH 1;
+
+CREATE TABLE Producers (
+  producer_id              NUMBER(10)  DEFAULT prod_seq.nextval NOT NULL,
+  producer_name            VARCHAR2(255)  NOT NULL);
+
+ALTER TABLE Producers ADD (
+  CONSTRAINT prod_pk PRIMARY KEY (producer_id));
+COMMIT;
+
+-- 3. ProducersPublishing
+CREATE SEQUENCE prodpubl_seq START WITH 1;
+
+CREATE TABLE ProducersPublishing (
+  prodpub_id              	NUMBER(10)  DEFAULT publ_seq.nextval NOT NULL,
+  producer_id				NUMBER(10)	NOT NULL,
+  publisher_id          	NUMBER(10)  NOT NULL);
+
+ALTER TABLE ProducersPublishing ADD (
+  CONSTRAINT prodpub_pk PRIMARY KEY (prodpub_id),
+  CONSTRAINT prod_fk FOREIGN KEY (producer_id)
+  REFERENCES Producers(producer_id),
+  CONSTRAINT publ_fk FOREIGN KEY (publisher_id)
+  REFERENCES Publishers(publisher_id));
+COMMIT;
